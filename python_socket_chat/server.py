1260698b3815e308002873a449435644c7f139c6
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-30 00:27:19 +0100

    Added the server initialization and listen

--- python_socket_chat/server.py
+++ python_socket_chat/server.py
@@ -1,3 +1,16 @@
+import socket
+
+
+def initiate():
+    ip_address = input("Please enter the server IPv4 address: ").strip()
+    port = int(input("Please enter the server port: "))
+    address = (ip_address, port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(address)
+    return s
+
 
 if __name__ == "__main__":
-    pass
+    s = initiate()
+    s.listen()
+    print(f"[STATUS] The server is listening on {s.getsockname()}")
