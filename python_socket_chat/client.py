ba3cca5074642d6681b056d74aeeecbfe6794f52
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-30 00:58:44 +0100

    Added the client read_server_address

--- python_socket_chat/client.py
+++ python_socket_chat/client.py
@@ -1,3 +1,7 @@
+from common import read_server_address
+
 
 if __name__ == "__main__":
-    pass
+    server_address = read_server_address()
+    print(
+        f"[STATUS] Trying to connect to {server_address[0]}:{server_address[1]}")
