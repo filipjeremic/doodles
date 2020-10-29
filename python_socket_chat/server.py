76dd492cf360e1afa98631d1611007ed42ceb16d
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-30 00:53:33 +0100

    Added the server_socket bind and listen

--- python_socket_chat/server.py
+++ python_socket_chat/server.py
@@ -1,12 +1,15 @@
 from common import read_server_address
+import socket
 
 
 if __name__ == "__main__":
     server_address = read_server_address()
-
-    #server_socket.listen()
-    #print(f"[STATUS] The server is listening on {server_socket.getsockname()}")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(server_address)
+    server_socket.listen()
+    print(
+        f"[STATUS] The server is listening on {server_socket.getsockname()[0]}:{server_socket.getsockname()[1]}")
 
     #client_socket, client_address = server_socket.accept()
-    #print(client_socket)
-    #print(client_address)
+    # print(client_socket)
+    # print(client_address)
