c5bb40abb3a2356e7823d53a48ec429802092b2b
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-30 01:08:54 +0100

    Renamed "server_socket" to "read_socket" and "client_socket" to "write_socket"

--- python_socket_chat/server.py
+++ python_socket_chat/server.py
@@ -4,12 +4,12 @@ import socket
 
 if __name__ == "__main__":
     server_address = read_server_address()
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(server_address)
-    server_socket.listen()
+    read_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    read_socket.bind(server_address)
+    read_socket.listen()
     print(
-        f"[STATUS] The server is listening on {server_socket.getsockname()[0]}:{server_socket.getsockname()[1]}")
+        f"[STATUS] The server is listening on {read_socket.getsockname()[0]}:{read_socket.getsockname()[1]}")
 
-    #client_socket, client_address = server_socket.accept()
+    # write_socket, client_address = read_socket.accept()
     # print(client_socket)
     # print(client_address)
