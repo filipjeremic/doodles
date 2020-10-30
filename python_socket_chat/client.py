45f1eef74d2a973f69cc11a69b41606187b249a2
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-30 01:12:53 +0100

    Added the client connection to the server

--- python_socket_chat/client.py
+++ python_socket_chat/client.py
@@ -1,7 +1,13 @@
 from common import read_server_address
+import socket
 
 
 if __name__ == "__main__":
     server_address = read_server_address()
     print(
         f"[STATUS] Trying to connect to {server_address[0]}:{server_address[1]}")
+
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.connect(server_address)
+    print(
+        f"[STATUS] Successfully connected to {my_socket.getsockname()}:{123}")
